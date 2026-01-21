e04dbb073d35a2c8fe4952d610f4f5ae71397c79
enderlink178576 <tvandewalle1@eduhdf.fr>
2026-01-21 14:56:55 +0100

    Update menu.js

--- menu/menu.js
+++ menu/menu.js
@@ -1,5 +1,5 @@
 // Charger le contenu de menu.html dans la div #menu
-fetch("/nav/menu.html")
+fetch("menu.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("menu").innerHTML = data;
