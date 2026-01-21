237f34a6bee8bc9081f94eb1ae203d8e74e8107d
enderlink178576 <tvandewalle1@eduhdf.fr>
2026-01-21 14:59:45 +0100

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,13 @@
+fetch("menu.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("menu").innerHTML = data;
+
+    // Active le bouton hamburger
+    const toggle = document.getElementById("menu-toggle");
+    const menuList = document.getElementById("menu-list");
+
+    toggle.addEventListener("click", () => {
+      menuList.classList.toggle("show");
+    });
+  });
