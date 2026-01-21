ca56ff701831c86fb26a28b734c322c5690eed0a
enderlink178576 <tvandewalle1@eduhdf.fr>
2026-01-21 14:05:07 +0100

    Update test.js

--- footer/test.js
+++ footer/test.js
@@ -1,1 +1,6 @@
-document.querySelector("#menu").style.display = "none";
+const toggle = document.getElementById("menu-toggle");
+    const menuList = document.getElementById("menu-list");
+
+    toggle.addEventListener("click", () => {
+      menuList.classList.toggle("show");
+    });
